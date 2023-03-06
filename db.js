0920a51ace98a88694a641b082b609980b82062b
lcrowther-snyk <lawrence.crowther@snyk.io>
2023-03-06 11:57:23 +1000

    fixed localhost issue

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 var Schema   = mongoose.Schema;
 
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://127.0.0.1:27017';
 const dbName = 'tic-tac-toe';
 mongoose.connect(url+"/"+dbName);
 
