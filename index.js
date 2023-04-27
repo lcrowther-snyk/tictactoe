541972ce63228c14a795dfcee1fa2fc31ed72b1a
lcrowther-snyk <lawrence.crowther@snyk.io>
2023-04-27 09:36:29 +0800

    rolled back path traversal hack (st)

--- index.js
+++ index.js
@@ -97,5 +97,5 @@ function requireAuth(req, res, next) {
     }
 }
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(3001, () => console.log('Server started on port 3001'));
 
